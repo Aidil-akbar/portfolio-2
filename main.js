1373022163be2cc29e32a9a582ffea7c11bbc8c3
aidil <m.aidil2333@gmail.com>
2024-08-09 22:40:22 +0700

    modified isi semua file

--- main.js
+++ main.js
@@ -1,17 +1,6 @@
-const navSlide = () => {
-  const burger = document.querySelector(".burger");
-  const navLists = document.querySelector("nav");
+const menuToggle = document.querySelector(".menu-toggle input");
+const nav = document.querySelector("nav ul");
 
-  burger.addEventListener("click", () => {
-    navLists.classList.toggle("nav-active");
-    burger.classList.toggle("toggle-burger");
-  });
-};
-
-navSlide();
-
-window.onbeforeunload = () => {
-  for (const form of document.getElementsByTagName("form")) {
-    form.reset();
-  }
-};
+menuToggle.addEventListener("click", function () {
+  nav.classList.toggle("slide");
+});
